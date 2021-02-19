afba0f9c1b74f87b8285aea015f86f622ee31770
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-19 15:41:24 +0600

    creat div, span

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let button = document.querySelector('button')
-let inputName = document.querySelector("#name")
-let inputMessage = document.querySelector("#message")
-let inputColor = document.querySelector("#color")
-button.addEventListener('click', function(){
-    console.log(inputName.value);
-    console.log(inputMessage.value);
-    console.log(inputColor.value);
+let button = document.querySelector('button');
+let inputName = document.querySelector("#name");
+let inputMessage = document.querySelector("#message");
+let inputColor = document.querySelector("#color");
 
+let div = document.createElement('div');
+let spanName = document.createElement('span');
+let spanMessage = document.createElement('span');
+
+button.addEventListener('click', function(){
+    console.log(inputName.value, inputMessage.value, inputColor.value);
+    let div = document.createElement('div');
+    let spanName = document.createElement('span');
+    let spanMessage = document.createElement('span');
+    document.body.append(div);
+    div.append(spanName);
+    div.append(spanMessage);
+    spanName.innerText = inputName.value + ": "
+    spanMessage.innerText = inputMessage.value
+    spanName.style.color = inputColor.value
+    spanName.style.fontWeight = "bold"
 })
 
